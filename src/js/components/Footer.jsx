4d7f4c3c7d37d8b047a67d232b2e9c5b150f4e33
Daniel <danolox@gmail.com>
2025-06-18 07:19:43 +0000

    Entrega proyecto

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+function Footer() {
+    return (
+
+            <div className="container-fluid bg-dark fixed-bottom d-flex align-items-center justify-content-center" style={{height: "60px"}}>
+
+                <p className="text-white mb-0">Copyright © This website 2025</p>
+
+            </div>
+    );
+};
+
+export default Footer;
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+function Jumbotron() {
+    return (
+            <div className="jumbotron bg-light rounded p-4">
+                <h1 className="display-3">A Warm Welcome!</h1>
+                <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab voluptatum, labore dolores enim quisquam facere in delectus soluta facilis fugit ratione illo, sapiente non error, suscipit quidem amet praesentium esse ad velit sit officiis. Illum dolor eius repellat aspernatur corporis.</p>
+                <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+            </div>
+    );
+};
+
+export default Jumbotron;
+
+
+
+
